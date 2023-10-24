9c7b4f810a3361086f367003c436c684069aaf23
Vanessa Trinh <xtourmaline@gmail.com>
2023-10-23 20:02:28 -0400

    able to display current month, day and year on the top of the page
    having issues with getting data from content of .time-block

--- script.js
+++ script.js
@@ -3,13 +3,17 @@
 // in the html.
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
-    $(".btn saveBtn col-2 col-md-1")
-
+    // clicking on save button saves content that user types in
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
+    $(".saveBtn").on("click", function() {
+        pass
+
+
+    });
 
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
@@ -17,10 +21,29 @@ $(function () {
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
     
+    
+
 
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    //
+    $(".time-block").each(function () {
+        if (parseInt($(this) == dayjs().format("HH A")) {
+            return "present"
+        } else if (parseInt($(this) > ayjs().format("HH A"))) {
+            return "future"
+        } else {
+           return "past"
+        }
+    });
+    
+
     // TODO: Add code to display the current date in the header of the page.
+    function displayCurrentTime() {
+        const currentDay = dayjs();
+        const formattedDate = currentDay.format("dddd, MMMM D, YYYY");
+        $("#currentDay").text(formattedDate);
+    }
+
+        displayCurrentTime();
 });
